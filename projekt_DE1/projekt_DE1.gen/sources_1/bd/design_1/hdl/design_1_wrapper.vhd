1c2501890f2151226eb9266863c771be6b885073
matejhroch <124818794+matejhroch@users.noreply.github.com>
2023-05-02 15:50:05 +0200

    final 1

---
+++ projekt_DE1/projekt_DE1.gen/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,23 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
+--Date        : Tue May  2 15:34:05 2023
+--Host        : PC-636 running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  end component design_1;
+begin
+design_1_i: component design_1
+ ;
+end STRUCTURE;
